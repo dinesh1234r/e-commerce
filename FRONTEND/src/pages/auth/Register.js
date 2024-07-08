40d041c9c9f1e0782debeabe6227ce8169247c64
dinesh1234r <107352596+dinesh1234r@users.noreply.github.com>
2024-07-08 15:57:21 +0530

    Update Register.js

--- FRONTEND/src/pages/auth/Register.js
+++ FRONTEND/src/pages/auth/Register.js
@@ -2,16 +2,12 @@ import React, { useContext } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 import '../auth/style.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Navigate, useNavigate } from 'react-router-dom'
 import { ShopContext } from '../../context/shopcontext'
-
-// function Auth() {
-//     return (
-//       <div className='auth'>
-//         <Register/>
-//       </div>
-//     )
-// }
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
 function Register(){
   const navigate=useNavigate()
@@ -25,31 +21,39 @@ function Register(){
       };
       axios.post("https://e-commerce-backend-l0au.onrender.com/user/register",postData)
       .then(response=>{
-        alert(response.data)
+        toast.success(response.data,{onClose:()=>{
+            navigate('/')
+          },autoClose:1500})
       })
       .catch(error=>{
         console.log('Error Occured in Register Form')
       })
       setUsername("")
       setPassword("")
-      navigate('/')
   }
 return (
-  <div className='auth'>
-  <div className='auth-container'>
-      <h1 >Register</h1>
-      <form>
-          <div className='form-group'>
-          <label htmlFor='username' >UserName:</label>
+  <div className='body'>
+  <div className='box'>
+      <div className='header'>
+        <div className='text'>Register</div>
+        <div className='underline'></div>
+      </div>
+      <form className='inputs'>
+          <div className='input'>
+          <label htmlFor='username' ><FontAwesomeIcon className='icon' icon={faUser} /></label>
           <input type='text' value={username} onChange={(event)=>setUsername(event.target.value)} id='username'/>
           </div>
-          <div>
-          <label htmlFor='password'>Password:</label>
+          <div className='input'>
+          <label htmlFor='password'><FontAwesomeIcon className='icon' icon={faLock} /></label>
           <input type='password' value={password} onChange={(event)=>setPassword(event.target.value)} id='password'/>
           </div>
-          <button type='submit' onClick={handlesubmit}>Submit</button>
+          <div className='submit'>
+          <button className='submit' type='submit' onClick={handlesubmit}>Submit</button>
+          </div>
+          
       </form>
   </div>
+  <ToastContainer/>
   </div>
 )
 }
