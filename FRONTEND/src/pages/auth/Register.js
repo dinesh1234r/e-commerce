843febf8c0f005fcd0a56bc21b41bf8eca55f687
DINESHKUMAR R <107352596+dinesh1234r@users.noreply.github.com>
2024-11-20 13:23:36 +0530

    Update Register using chakra ui

--- FRONTEND/src/pages/auth/Register.js
+++ FRONTEND/src/pages/auth/Register.js
@@ -4,7 +4,10 @@ import axios from 'axios'
 import '../auth/style.css'
 import { Navigate, useNavigate } from 'react-router-dom'
 import { ShopContext } from '../../context/shopcontext'
-
+import { FaUser, FaLock } from 'react-icons/fa';
+import { Box, Button, Heading, HStack, Input, VStack } from '@chakra-ui/react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Register(){
   const navigate=useNavigate()
@@ -16,34 +19,42 @@ function Register(){
         username: username,
         password: password
       };
-      axios.post("http://localhost:9000/user/register",postData)
+      axios.post("https://e-commerce-backend-l0au.onrender.com/user/register",postData)
       .then(response=>{
-        alert(response.data)
+        if(response.data==="User already existes")
+        {
+          toast.error(response.data)
+        }
+        else
+        {
+          toast.success(response.data,{
+            onClose:navigate('/')
+          })
+        }
       })
       .catch(error=>{
-        console.log('Error Occured in Register Form')
+        toast.error('Error !!!')
       })
       setUsername("")
       setPassword("")
-      navigate('/')
+      
   }
 return (
-  <div className='auth'>
-  <div className='auth-container'>
-      <h1 >Register</h1>
-      <form>
-          <div className='form-group'>
-          <label htmlFor='username' >UserName:</label>
-          <input type='text' value={username} onChange={(event)=>setUsername(event.target.value)} id='username'/>
-          </div>
-          <div>
-          <label htmlFor='password'>Password:</label>
-          <input type='password' value={password} onChange={(event)=>setPassword(event.target.value)} id='password'/>
-          </div>
-          <button type='submit' onClick={handlesubmit}>Submit</button>
-      </form>
-  </div>
-  </div>
+  <Box w={'30%'} mx={'auto'} mt={'25vh'} boxShadow={'lg'}>
+      <Heading mb={4} p={4}>Register</Heading>
+      <VStack gap={4}>
+          <HStack >
+          <FaUser style={{marginRight:'10px'}}/>
+          <Input type='text' value={username} onChange={(event)=>setUsername(event.target.value)} id='username'/>
+          </HStack>
+          <HStack>
+          <FaLock style={{marginRight:'10px'}}/>
+          <Input type='password' value={password} onChange={(event)=>setPassword(event.target.value)} id='password'/>
+          </HStack>
+          <Button colorScheme='blue' type='submit' onClick={handlesubmit} mb={4}>Submit</Button>
+      </VStack>
+      <ToastContainer/>
+  </Box>
 )
 }
 
